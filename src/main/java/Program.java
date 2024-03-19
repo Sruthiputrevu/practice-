d9acc8aba71f5b53382d643f95a83d2bdc5acbbd
Sruthi Putrevu <sruthipvn@gmail.com>
2024-03-19 15:27:55 +0530

    Submitting the code.

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+package com.qualizeal.javaapp.main;
+
+    import com.qualizeal.javaapp.mathematics.Subtraction;
+	    public class Program {
+		    public static void main(String[] args){
+			    Subtraction subtraction = new Subtraction();
+			    subtraction.sub();
+			}
+        }			
+	
---
+++ src/main/java/Subtraction.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp.mathematics;
+
+    public class Subtraction{
+        public void sub(){
+		
+		int a = 20;
+		int b = 15;
+		int c = 10;
+		int d = 5;
+		
+		int result = a - b;
+		System.out.println(result);
+		}
+	}
+		
